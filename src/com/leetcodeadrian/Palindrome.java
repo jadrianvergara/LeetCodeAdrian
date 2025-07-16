386c5924e7e6e7ad76831a92f8ba5ecb43323bb9
Adrian <jaz.x.prime@gmail.com>
2025-07-15 20:35:13 -0400

    Update Palindrome.java

--- src/com/leetcodeadrian/Palindrome.java
+++ src/com/leetcodeadrian/Palindrome.java
@@ -46,8 +46,17 @@ public class Palindrome {
         return true;
     }
     
+    // best solution
     private static boolean isPalindromeNoConvert(int x) {
-    	return true;
+        if (x < 0) return false;
+        int original = x;
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
     }
 
 
